92b9c09aa7fca1e40a4c692328f96f805ba4d7ec
Donnie Adams <donnie@acorn.io>
2023-11-02 16:16:32 -0400

    Add a few map functions
    Signed-off-by: Donnie Adams <donnie@acorn.io>

--- z.go
+++ z.go
@@ -39,3 +39,31 @@ func Dereference[T any](p *T) (v T) {
 	}
 	return
 }
+
+// AddToMap will add the key value pair to the map, ensuring that the map is not nil.
+func AddToMap[K comparable, V any](m map[K]V, k K, v V) map[K]V {
+	return ConcatMaps(m, map[K]V{k: v})
+}
+
+// ConcatMaps will iteratively add all the key/value pairs from each map, overwriting existing keys.
+// That is, if every map in ms has the same key, then the value of that key in the resulting map will be
+// the value of the key in the last map in ms.
+// If no maps are provided, then a nil map is returned.
+// If at least one map is provided (nil or not), then a non-nil map is returned.
+func ConcatMaps[K comparable, V any](ms ...map[K]V) map[K]V {
+	if len(ms) == 0 {
+		return nil
+	}
+
+	m := ms[0]
+	if m == nil {
+		m = make(map[K]V)
+	}
+	for i := 1; i < len(ms); i++ {
+		for k, v := range ms[i] {
+			m[k] = v
+		}
+	}
+
+	return m
+}
